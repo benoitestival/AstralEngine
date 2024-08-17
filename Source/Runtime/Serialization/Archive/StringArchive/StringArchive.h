d801bfce8f10cd7632ce62ac285c71c035a5aa5d
BenoitEstival <benoit.estival@hotmail.com>
2024-08-17 23:52:46 +0200

    Start archive rework AGAIN

--- Source/Runtime/Serialization/Archive/StringArchive/StringArchive.h
+++ Source/Runtime/Serialization/Archive/StringArchive/StringArchive.h
@@ -1,10 +1,12 @@
 ﻿#pragma once
 #include <string>
 
-#include "StringArchiveNode.h"
+//#include "StringArchiveNode.h"
 #include <unordered_map>
 
+#include "../../../Utils/Map.h"
 #include "../../../Utils/TemplateUtils.h"
+#include "../../Utils/SerializationUtils.h"
 
 class FStringArchive {
 public:
@@ -14,25 +16,44 @@ public:
     void WriteKey(const std::string& DataKey);
     void ReadKey(const std::string& DataKey);
     
-    template<class T>
-    void WriteData(const T& Data) {
-        Cast<FStringArchiveLeafNode>(ArchiveRootNode->GetActiveNode())->InsertDataInNode(Data);
-    }
-
-    template<class T>
-    void ReadData(const T& Data) {
-        Cast<FStringArchiveLeafNode>(ArchiveRootNode->GetActiveNode())->ReadDataInNode(Data);
-    }
+    // template<class T>
+    // void WriteData(const T& Data) {
+    //     Cast<FStringArchiveLeafNode>(ArchiveRootNode->GetActiveNode())->InsertDataInNode(Data);
+    // }
+    //
+    // template<class T>
+    // void ReadData(const T& Data) {
+    //     Cast<FStringArchiveLeafNode>(ArchiveRootNode->GetActiveNode())->ReadDataInNode(Data);
+    // }
 
     void Option(EArchiveAction ArchiveAction);
 
     
 private:
 
-    FStringArchiveRootNode* ArchiveRootNode;
+    //FStringArchiveRootNode* ArchiveRootNode;
 };
 
 
-// class FStringArchiveNode {
-//     std::unordered_map<int, std::string> test = {{}};
-// };
+class FStringArchiveNode {
+public:
+    
+    template<SupportStringSerialization T>
+    void InsertDataInNode(const std::string& Key, T& Data) {
+        FStream Stream = FStream();
+        Stream.Stream() << Data;
+        NodeDatas.Insert({Key, Stream.ToString()});
+    }
+
+    template<SupportStringSerialization T>
+    void ReadDataInNode(const std::string& Key, T& Data) {
+        FStream Stream = FStream();
+        Stream.Stream() << NodeDatas.Find(Key);
+        Data = Stream.Stream();
+    }
+private:
+    TMap<std::string, std::string> NodeDatas;
+
+    TMap<std::string, FStringArchiveNode> SubNodes;
+    
+};
