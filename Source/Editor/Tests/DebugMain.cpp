89f65b135342116fb64ff1271a77ebfabbb36d51
BenoitEstival <benoit.estival@hotmail.com>
2026-09-25 18:07:09 +0200

    Binary Ar funcionnal

--- Source/Editor/Tests/DebugMain.cpp
+++ Source/Editor/Tests/DebugMain.cpp
@@ -4,16 +4,9 @@
 #include <iostream>
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
-
 #include <glm/gtx/transform.hpp>
 
-#include "../../Runtime/Engine/CoreObjects/Utils/ObjectCoreUtility.h"
-#include "../../Runtime/Engine/Engine/Engine.h"
-#include "../../Runtime/Engine/Inputs/Components/InputComponent.h"
-#include "../../Runtime/Engine/Inputs/Objects/InputAction.h"
-#include "../../Runtime/Engine/Inputs/Systems/InpuManager.h"
-#include "../../Runtime/Engine/Statics/GameplayStatics.h"
-#include "../../Runtime/Serialization/Archive/Implementations/JsonArchive.h"
+
 
 int ADebugMain::DebugMain() {
     
